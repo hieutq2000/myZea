f7537860a521a559de770162a9f999b25927964b
Hoàng Minh Hiếu <hieuiospubgm@gmail.com>
2025-12-14 01:40:34 +0700

    Bump version to 1.0.3 to trigger new IPA OTA build

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "myZyea",
         slug: "vinalive-ai-mobile",
         scheme: "zyea", // URL scheme for deep linking
-        version: "1.0.2",
+        version: "1.0.3",
         orientation: "portrait",
         icon: "./assets/icon.png",
         userInterfaceStyle: "light",
