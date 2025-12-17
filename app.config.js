72f8da4c0c923501b66ac461aa73bf972cc24cc6
Hoàng Minh Hiếu <hieuiospubgm@gmail.com>
2025-12-18 04:37:43 +0700

    Bump version to 1.0.5 - trigger iOS build with dark icon

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "myZyea",
         slug: "vinalive-ai-mobile",
         scheme: "zyea", // URL scheme for deep linking
-        version: "1.0.4",
+        version: "1.0.5",
         orientation: "portrait",
         icon: "./assets/icon.png",
         userInterfaceStyle: "automatic",
